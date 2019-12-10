061407fb4b789aaa7e981abcaca20b044274a934
rmiri <re_miriuk@hotmail.com>
2019-12-10 11:25:08 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+    def initializate(name)
+        @name = name
+    end
+    def name
+        @name
+    end
+    def name=(sayMyName)
+        @name = sayMyName
+    end
+    def breed=(dogBreed)
+        @breed = dogBreed
+    end
+    def breed
+        @breed
+    end
+end
